28b7b4f5297a75007179a1ef79118acbb1c92eca
angeloRobson <angelorobsonmelo@gmail.com>
2019-07-22 21:46:02 -0300

    add endless scrolling

---
+++ app/src/main/java/in/galaxyofandroid/searchablespinner/MainActivityPaged.java
@@ -0,0 +1,98 @@
+package in.galaxyofandroid.searchablespinner;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+import in.galaxyofandroid.spinerdialog.OnAfterTextChangedListener;
+import in.galaxyofandroid.spinerdialog.OnLoadMore;
+import in.galaxyofandroid.spinerdialog.OnSpinerItemClick;
+import in.galaxyofandroid.spinerdialog.SpinnerDialogPaged;
+
+public class MainActivityPaged extends AppCompatActivity {
+
+    ArrayList<String> items = new ArrayList<>();
+    SpinnerDialogPaged mSpinnerDialogPaged;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final TextView selectedItems = (TextView) findViewById(R.id.txt);
+
+
+        items.add("Mumbai");
+        items.add("Delhi");
+        items.add("Bengaluru");
+        items.add("Hyderabad");
+        items.add("Ahmedabad");
+        items.add("Chennai");
+        items.add("Kolkata");
+        items.add("Surat");
+        items.add("Pune");
+        items.add("Jaipur");
+        items.add("Lucknow");
+        items.add("Maceió");
+        items.add("Recife");
+        items.add("Pernambuco");
+        items.add("Macapá");
+        items.add("Fortaleza");
+        items.add("Tocantins");
+        items.add("Atalaia");
+        items.add("Satuba");
+        items.add("Pilar");
+
+        mSpinnerDialogPaged = new SpinnerDialogPaged(MainActivityPaged.this, items,
+                "Select");
+
+        mSpinnerDialogPaged.setTitleColor(getResources().getColor(R.color.colorAccent));
+        mSpinnerDialogPaged.setSearchIconColor(getResources().getColor(R.color.colorAccent));
+        mSpinnerDialogPaged.setSearchTextColor(getResources().getColor(R.color.colorAccent));
+        mSpinnerDialogPaged.setItemColor(getResources().getColor(R.color.colorAccent));
+        mSpinnerDialogPaged.setItemDividerColor(getResources().getColor(R.color.colorAccent));
+        mSpinnerDialogPaged.setCloseColor(getResources().getColor(R.color.colorAccent));
+
+        mSpinnerDialogPaged.setCancellable(true);
+        mSpinnerDialogPaged.setShowKeyboard(false);
+
+        mSpinnerDialogPaged.bindOnSpinerListener(new OnSpinerItemClick() {
+            @Override
+            public void onClick(String item, int position) {
+                Toast.makeText(MainActivityPaged.this, item + "  " + position + "", Toast.LENGTH_SHORT).show();
+                selectedItems.setText(item + " Position: " + position);
+            }
+        });
+
+        // set threshold
+        mSpinnerDialogPaged.setVisibleThreshold(5);
+        // hide or show search field with true of false
+        mSpinnerDialogPaged.setHideSearchField(false);
+
+        mSpinnerDialogPaged.bindOnLoadMoreListener(new OnLoadMore() {
+            @Override
+            public void onLoadMore(int page) {
+                // call api request
+            }
+        });
+
+        mSpinnerDialogPaged.bindOnAfterTextChanged(new OnAfterTextChangedListener() {
+            @Override
+            public void onAfterTextChanged(String term) {
+
+            }
+        });
+
+        findViewById(R.id.show).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mSpinnerDialogPaged.showSpinerDialog();
+            }
+        });
+    }
+
+}
---
+++ spinerdialog/src/main/java/in/galaxyofandroid/spinerdialog/SpinnerDialogPaged.java
@@ -0,0 +1,274 @@
+package in.galaxyofandroid.spinerdialog;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.graphics.drawable.ColorDrawable;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Ângelo Robson on 22/07/2019.
+ */
+
+public class SpinnerDialogPaged {
+
+    ArrayList<String> items;
+    Activity context;
+    String dTitle, closeTitle = "Close";
+    OnSpinerItemClick onSpinerItemClick;
+    OnLoadMore onLoadMore;
+    AlertDialog alertDialog;
+    int pos;
+    int style;
+    boolean cancellable = false;
+    boolean showKeyboard = false;
+    boolean useContainsFilter = false;
+    int titleColor, searchIconColor, searchTextColor, itemColor, itemDividerColor, closeColor;
+    private ArrayAdapterWithContainsFilter<String> adapter;
+    private int visibleThreshold = 5;
+    private boolean isHideSearchField;
+    private OnAfterTextChangedListener onAfterTextChangedListener;
+
+    private void initColor(Context context) {
+        this.titleColor = context.getResources().getColor(R.color.colorBlack);
+        this.searchIconColor = context.getResources().getColor(R.color.colorBlack);
+        this.searchTextColor = context.getResources().getColor(R.color.colorBlack);
+        this.itemColor = context.getResources().getColor(R.color.colorBlack);
+        this.closeColor = context.getResources().getColor(R.color.colorBlack);
+        this.itemDividerColor = context.getResources().getColor(R.color.colorLightGray);
+    }
+
+    public SpinnerDialogPaged(Activity activity, ArrayList<String> items, String dialogTitle) {
+        this.items = items;
+        this.context = activity;
+        this.dTitle = dialogTitle;
+        initColor(context);
+    }
+
+    public SpinnerDialogPaged(Activity activity, ArrayList<String> items, String dialogTitle, String closeTitle) {
+        this.items = items;
+        this.context = activity;
+        this.dTitle = dialogTitle;
+        this.closeTitle = closeTitle;
+        initColor(context);
+    }
+
+    public SpinnerDialogPaged(Activity activity, ArrayList<String> items, String dialogTitle, int style) {
+        this.items = items;
+        this.context = activity;
+        this.dTitle = dialogTitle;
+        this.style = style;
+        initColor(context);
+    }
+
+    public SpinnerDialogPaged(Activity activity, ArrayList<String> items, String dialogTitle, int style, String closeTitle) {
+        this.items = items;
+        this.context = activity;
+        this.dTitle = dialogTitle;
+        this.style = style;
+        this.closeTitle = closeTitle;
+        initColor(context);
+    }
+
+    public void bindOnSpinerListener(OnSpinerItemClick onSpinerItemClick1) {
+        this.onSpinerItemClick = onSpinerItemClick1;
+    }
+
+    public void bindOnLoadMoreListener(OnLoadMore onLoadMore) {
+        this.onLoadMore = onLoadMore;
+    }
+
+    public void bindOnAfterTextChanged(OnAfterTextChangedListener onAfterTextChangedListener) {
+        this.onAfterTextChangedListener = onAfterTextChangedListener;
+    }
+
+    public void showSpinerDialog() {
+        AlertDialog.Builder adb = new AlertDialog.Builder(context);
+        View v = context.getLayoutInflater().inflate(R.layout.dialog_layout, null);
+        TextView rippleViewClose = (TextView) v.findViewById(R.id.close);
+        TextView title = (TextView) v.findViewById(R.id.spinerTitle);
+        ImageView searchIcon = (ImageView) v.findViewById(R.id.searchIcon);
+        rippleViewClose.setText(closeTitle);
+        title.setText(dTitle);
+        final ListView listView = (ListView) v.findViewById(R.id.list);
+
+        ColorDrawable sage = new ColorDrawable(itemDividerColor);
+        listView.setDivider(sage);
+        listView.setDividerHeight(1);
+
+        final EditText searchBox = (EditText) v.findViewById(R.id.searchBox);
+        if (isShowKeyboard()) {
+            showKeyboard(searchBox);
+        }
+
+        if (isHideSearchField) {
+            searchIcon.setVisibility(View.INVISIBLE);
+            searchBox.setVisibility(View.INVISIBLE);
+        }
+
+        title.setTextColor(titleColor);
+        searchBox.setTextColor(searchTextColor);
+        rippleViewClose.setTextColor(closeColor);
+        searchIcon.setColorFilter(searchIconColor);
+
+        adapter = new ArrayAdapterWithContainsFilter<String>(context, R.layout.items_view, items) {
+            @NonNull
+            @Override
+            public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+                View view = super.getView(position, convertView, parent);
+                TextView text1 = view.findViewById(R.id.text1);
+                text1.setTextColor(itemColor);
+                return view;
+            }
+        };
+        listView.setAdapter(adapter);
+        listView.setOnScrollListener(new EndlessScrollListener(onLoadMore, visibleThreshold));
+        adb.setView(v);
+        alertDialog = adb.create();
+        alertDialog.getWindow().getAttributes().windowAnimations = style;//R.style.DialogAnimations_SmileWindow;
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                TextView t = (TextView) view.findViewById(R.id.text1);
+                for (int j = 0; j < items.size(); j++) {
+                    if (t.getText().toString().equalsIgnoreCase(items.get(j).toString())) {
+                        pos = j;
+                    }
+                }
+                onSpinerItemClick.onClick(t.getText().toString(), pos);
+                closeSpinerDialog();
+            }
+        });
+
+        searchBox.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                onAfterTextChangedListener.onAfterTextChanged(searchBox.getText().toString());
+            }
+        });
+
+        rippleViewClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                closeSpinerDialog();
+            }
+        });
+        alertDialog.setCancelable(isCancellable());
+        alertDialog.setCanceledOnTouchOutside(isCancellable());
+        alertDialog.show();
+    }
+
+    public void addMoreItemns(ArrayList<String> items) {
+        this.items.addAll(items);
+        adapter.notifyDataSetChanged();
+    }
+
+    public void setVisibleThreshold(int visibleThreshold) {
+        this.visibleThreshold = visibleThreshold;
+    }
+
+    public void closeSpinerDialog() {
+        hideKeyboard();
+        if (alertDialog != null) {
+            alertDialog.dismiss();
+        }
+    }
+
+    private void hideKeyboard() {
+        try {
+            InputMethodManager inputManager = (InputMethodManager) context.getSystemService(Context.INPUT_METHOD_SERVICE);
+            inputManager.hideSoftInputFromWindow(context.getCurrentFocus().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+        } catch (Exception e) {
+        }
+    }
+
+    private void showKeyboard(final EditText ettext) {
+        ettext.requestFocus();
+        ettext.postDelayed(new Runnable() {
+                               @Override
+                               public void run() {
+                                   InputMethodManager keyboard = (InputMethodManager) context.getSystemService(Context.INPUT_METHOD_SERVICE);
+                                   keyboard.showSoftInput(ettext, 0);
+                               }
+                           }
+                , 200);
+    }
+
+    private boolean isCancellable() {
+        return cancellable;
+    }
+
+    public void setHideSearchField(boolean ishideSearchField) {
+        this.isHideSearchField = ishideSearchField;
+    }
+
+    public void setCancellable(boolean cancellable) {
+        this.cancellable = cancellable;
+    }
+
+    private boolean isShowKeyboard() {
+        return showKeyboard;
+    }
+
+    private boolean isUseContainsFilter() {
+        return useContainsFilter;
+    }
+
+
+    public void setShowKeyboard(boolean showKeyboard) {
+        this.showKeyboard = showKeyboard;
+    }
+
+    public void setUseContainsFilter(boolean useContainsFilter) {
+        this.useContainsFilter = useContainsFilter;
+    }
+
+    public void setTitleColor(int titleColor) {
+        this.titleColor = titleColor;
+    }
+
+    public void setSearchIconColor(int searchIconColor) {
+        this.searchIconColor = searchIconColor;
+    }
+
+    public void setSearchTextColor(int searchTextColor) {
+        this.searchTextColor = searchTextColor;
+    }
+
+    public void setItemColor(int itemColor) {
+        this.itemColor = itemColor;
+    }
+
+    public void setCloseColor(int closeColor) {
+        this.closeColor = closeColor;
+    }
+
+    public void setItemDividerColor(int itemDividerColor) {
+        this.itemDividerColor = itemDividerColor;
+    }
+
+}
